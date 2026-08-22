bc8f091f6149cf51594db0f0e6f009d261f6ae93
Sabuj <sabuj.sangram@nevaehtech.com>
2026-08-23 01:26:38 +0530

    invoice schema

---
+++ src/models/invoice.model.ts
@@ -0,0 +1,46 @@
+import { Schema, model, Types } from "mongoose";
+import { IInvoice } from "@/interfaces/invoice.interface";
+
+const invoiceSchema = new Schema<IInvoice>({
+    patientId: {
+        type: Types.ObjectId,
+        ref: "Patient",
+        required: true
+    },
+    items: {
+        type: [
+            {
+                name: String,
+                price: Number,
+                quantity: Number,
+                discount: Number,
+                total: Number,
+            }
+        ],
+        required: true
+    },
+    totalAmount: {
+        type: Number,
+        required: true
+    },
+    discount: {
+        type: Number,
+        required: true
+    },
+    finalAmount: {
+        type: Number,
+        required: true
+    },
+    status: {
+        type: String,
+        enum: ["PAID", "UNPAID"],
+        default: "UNPAID"
+    },
+    branchId: {
+        type: Types.ObjectId,
+        ref: "Branch",
+        required: true
+    }
+}, { timestamps: true });
+
+export default model<IInvoice>("Invoice", invoiceSchema);
