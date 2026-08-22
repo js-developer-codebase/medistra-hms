f267b3839fa252ae8b56f2edfcaf36cff1c71feb
Sabuj <sabuj.sangram@nevaehtech.com>
2026-08-22 20:10:43 +0530

    oanization model

--- src/interfaces/organization.interface.ts
+++ src/interfaces/organization.interface.ts
@@ -1,6 +1,7 @@
 import { Document, Types } from "mongoose";
 export interface IOrganization extends Document {
     organizationName: string;
+    organizationId: string,
     organizationType: ('HOSPITAL' | 'CLINIC' | 'PHARMACY');
     headQuarter?: Types.ObjectId;
     branchType: ('MAIN' | 'BRANCH');
---
+++ src/models/organization.mode.ts
@@ -0,0 +1,49 @@
+import { Schema, model, Types } from "mongoose";
+import { IOrganization } from "../interfaces/organization.interface";
+
+
+const organizationSchema = new Schema<IOrganization>(
+    {
+        organizationName: {
+            type: String,
+            required: true
+        },
+        organizationId: {
+            type: String,
+            required: true
+        },
+        organizationType: {
+            type: String,
+            enum: ['HOSPITAL', 'CLINIC', 'PHARMACY'],
+            required: true
+        },
+        headQuarter: {
+            type: Types.ObjectId,
+            ref: 'Organization'
+        },
+        branchType: {
+            type: String,
+            enum: ['MAIN', 'BRANCH'],
+            default: "MAIN",
+            required: true
+        },
+        email: {
+            type: String,
+        },
+        phone: {
+            type: String,
+        },
+        address: {
+            type: String,
+        },
+        logo: {
+            type: String
+        },
+        isActive: {
+            type: Boolean,
+            default: true
+        }
+    }, { timestamps: true })
+
+const Organization = model<IOrganization>('Organization', organizationSchema);
+export default Organization;
